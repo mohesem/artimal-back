26d3f48b1562b8e46277e6e584d23a0511b7efb7
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-06-28 13:16:47 +0430

    last commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import Cors from 'cors';
 import debug from 'debug';
+import logger from 'morgan';
+
 require('./DB/db');
 import routes from './routes/routers';
 
@@ -12,6 +14,7 @@ const port = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(logget('tiny'));
 app.use(Cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
